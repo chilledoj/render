60eb0efccb5bf9f71810b03e92a72427adb71a38
ojackman <chilledoj@gmail.com>
2017-04-10 22:28:15 +0100

    Summed up in one function?

--- render.go
+++ render.go
@@ -2,3 +2,29 @@
 Package render is a buffered renderer for Golang html/template
 */
 package render
+
+import (
+	"bytes"
+	"html/template"
+	"io"
+	"sync"
+)
+
+var globalBufPool = sync.Pool{
+	New: func() interface{} {
+		return new(bytes.Buffer)
+	},
+}
+
+// BufferedRender is a shorthand way of buffering render to
+func BufferedRender(tmpl *template.Template, w io.Writer, name string, data interface{}) error {
+	b := globalBufPool.Get().(*bytes.Buffer)
+	defer globalBufPool.Put(b)
+	b.Reset()
+
+	if err := tmpl.ExecuteTemplate(w, name, data); err != nil {
+		return err
+	}
+	b.WriteTo(w)
+	return nil
+}
